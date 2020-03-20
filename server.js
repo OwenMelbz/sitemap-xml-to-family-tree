7f2f4343d51820f60af10e30ab3ed5ba5c2a3326
Owen Melbourne <owenmelbz@gmail.com>
2020-03-20 23:54:16 +0000

    🔨 WIP

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.get('/scan', async (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
