075e5da4fd31182ba387ea557d451ae67131a90e
Owen Melbourne <owenmelbz@gmail.com>
2020-03-21 00:04:32 +0000

    🐛 Bug

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const extractPages = async () => {
 };
 
 const screenshotPages = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.setViewport({
         width: 1024,
